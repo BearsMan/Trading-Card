db66208b5af56254173dd7e8fd3afe8978588484
Darius Ronkowitz <dronkowitz@yahoo.com>
2023-09-28 17:49:56 -0400

    `Updated Trading Card - Game`
    Added more functions, and variables.
    Fixed Issues with the card not showing, and reading the proper data.
    Added Background Images.
    Added URL Code to show random images of something that does not exist within the URL. The website used is: ```URL = "https://thispersondoesnotexist.com/image"```.

--- Background.py
+++ Background.py
@@ -1,8 +1,33 @@
 import random
 from PIL import Image, ImageDraw
 
-from main import HEIGHT, WIDTH
 
+# Create an image for the card.
 image = Image.new("RGB", (750, 1050))
-WIDTH,HEIGHT = image.size
-num_rect = 
+WIDTH, HEIGHT = image.size
+num_rect = random.randint(50, 100)
+draw_image = ImageDraw.Draw(image)
+
+# for loops for width, and height.
+for i in range(num_rect):
+# create a rectangle shape.
+  rec_width = random.randint(10, 100)
+  rec_height = random.randint(10, 100)
+  rec_x = random.randint(0, WIDTH)
+  rec_y = random.randint(0, HEIGHT)
+
+  rec_shape = [
+    (rec_x, rec_y), 
+    (rec_x + rec_width, rec_y + rec_height)
+  ]
+
+# Draw rectangle
+  draw_image.rectangle(
+    rec_shape, 
+    fill = (
+      random.randint(0, 255), 
+      random.randint(0, 255), 
+      random.randint(0, 255)
+    )
+  )
+image.save("background.jpg")
--- main.py
+++ main.py
@@ -1,16 +1,25 @@
 from os import name
 import random
+from re import I
+import requests
 from PIL import Image, ImageDraw, ImageFont
 from name import NameSelector
+import Background
 
 image = Image.new("RGB", (750, 750), (0,0,0))
 fnt_title = ImageFont.truetype("LucidaTypewriterBold.ttf", 70)
 fnt_stat = ImageFont.truetype("LucidaTypewriterBold.ttf", 30)
 
 # global variables
-HEIGHT,WIDTH = image.size
+HEIGHT, WIDTH = image.size
 draw_image = ImageDraw.Draw(image)
+Background_image = Image.open("background.jpg")
+image.paste(Background_image), (0, 0)
 
+URL = "https://thispersondoesnotexist.com/image"
+person_image = Image.open(requests.get(URL, stream = True).raw)
+person_resize = person_image.resize((650, 550))
+image.paste(person_resize, (50, 100))
 main_Rect = [
   (50, 100), 
   (WIDTH - 50, HEIGHT - 400)
