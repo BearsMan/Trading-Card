9f0e4c2e9d0c4d92b6416ece2fa84ac5314c74db
Darius Ronkowitz <dronkowitz@yahoo.com>
2023-10-12 19:01:04 -0400

    `Updated Card Game`
    Fixed minor bugs with code.
    Added ```"Star.png"```
    Fixed bug with the red square (now is positioned correctly.)

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ import Background
 from bs4 import BeautifulSoup
 
 image = Image.new("RGB", (750, 750), (0,0,0))
-fnt_title = ImageFont.truetype("LucidaTypewriterBold.ttf", 70)
+fnt_title = ImageFont.truetype("LucidaTypewriterBold.ttf", 45)
 fnt_stat = ImageFont.truetype("LucidaTypewriterBold.ttf", 30)
 
 # global variables
@@ -16,6 +16,8 @@ HEIGHT, WIDTH = image.size
 draw_image = ImageDraw.Draw(image)
 Background_image = Image.open("background.jpg")
 image.paste(Background_image), (0, 0)
+rarity_value = random.randint(1, 9)
+
 
 # URL = "https://thispersondoesnotexist.com/image"
 # person_image = Image.open(requests.get(URL, stream = True).raw)
@@ -34,10 +36,10 @@ while is_image == False:
     img = Image.open(requests.get(findImage, stream = True).raw)
     is_image = True
   except:
-    print("image was not found.")
+    print("image was not found")
 img.save('character.png')
 person_img = Image.open('character.png')
-person_resize = person_img.resize((650, 550))
+person_resize = person_img.resize((635, 250))
 image.paste(person_resize, (50, 100))
 main_Rect = [
   (50, 100), 
@@ -48,13 +50,18 @@ draw_image.rectangle(
   outline = "red", 
   width = 15
 )
+star_image = Image.open("star.png").convert("RGBA")
+star_resize = star_image.resize((35, 30))
+for idx, i in enumerate(range(rarity_value)):
+  
+  image.paste(star_resize, (main_Rect[0][0] +(idx * 50), main_Rect[0][1] + 560), star_resize)
 bannerRectangle = [(50, HEIGHT - 100), (WIDTH - 50, HEIGHT - 50)]
 draw_image.rectangle(bannerRectangle, fill = "white")
-draw_image.text((50, HEIGHT - 300), "Strength \n 24",
+draw_image.text((50, HEIGHT - 300), "Strength \n" + str(random.randint(1, 99)),
 font = fnt_stat, align = "center")
 draw_image.text((WIDTH / 2 - 75, HEIGHT - 300),
-"Agility \n 30", font = fnt_stat, align = "center")
-draw_image.text((WIDTH - 175, HEIGHT - 300), "Defense \n 99",
+"Agility \n" + str(random.randint(1, 99)), font = fnt_stat, align = "center")
+draw_image.text((WIDTH - 175, HEIGHT - 300), "Defense \n" + str(random.randint(1, 99)),
 font = fnt_stat, align = "center")
 name = NameSelector().get_name()
 draw_image.text(
@@ -62,5 +69,9 @@ draw_image.text(
   name,
   font = fnt_title
 )
-
+star_image = Image.open("star.png").convert("RGBA")
+star_resize = star_image.resize((35, 30))
+for idx, i in enumerate(range(rarity_value)):
+  
+  image.paste(star_resize, (main_Rect[0][0] +(idx * 50), main_Rect[0][1] + 560), star_resize)
 image.save("fullcard.jpg")
